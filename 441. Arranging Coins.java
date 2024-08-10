ca88c031acad8dfa98ebe501bb2134717dab1557
Rishy-fishy <kumar1023rishav@gmail.com>
2024-08-11 02:35:30 +0530

    Add files via upload

---
+++ 441. Arranging Coins.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        if(n<=1)
+         return 1;
+        if(n<=3)
+         return (n==3)?2:1;
+        long s=1,e=n/2;
+        while(s<=e)
+        {
+            long m=s+(e-s)/2;
+            long t=m*(m+1)/2;
+            if(t>n)
+             e=m-1;
+            else if(t<n)
+             s=m+1;
+            else if(t==n)
+             return (int)m;
+        }
+        return (int)e;
+    }
+}
+/*
+for 1 stair number of coins required 1.
+for 2 stairs number of coins required 1+2.
+for 3 stairs number of coins required 1+2+3.
+for 4 stairs number of coins required 1+2+3+4.
+for 5 stairs number of coins required 1+2+3+4+5.
+for n stairs number of coins required n(n+1)/2.
+ */
